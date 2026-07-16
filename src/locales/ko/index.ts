b75cad95f74ab8a59978bc5963bd80988883eecb
WangShengQiang <109484028@qq.com>
2026-07-16 21:50:43 +0800

    feat: add about page i18n for all 5 languages (zh/en/ja/ko/es)

--- src/locales/ko/index.ts
+++ src/locales/ko/index.ts
@@ -2,5 +2,6 @@ import common from './common'
 import seo from './seo'
 import svgConverter from './svgConverter'
 import privacy from './privacy'
+import about from './about'
 
-export default { common, seo, pages: { svgConverter, privacy } }
+export default { common, seo, pages: { svgConverter, privacy, about } }
